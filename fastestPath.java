6ad4d567dd1b22073f94c7ece72face9ab6e696a
TevinNan <tevinnan@gmail.com>
2023-08-19 22:07:07 +0800

    cp

---
+++ fastestPath.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class fastestPath {
+    // private State start;
+    private double cost;
+    final int SATE_DIST = 1;
+    final int MAP_SIZE = 20;
+
+    // private ArrayList<Node> path = new ArrayList<Node>();
+
+}
